26d3fc258f6e8ac147bc85e32d3ae93561235af5
Chit-Senghang <senghang.chit@igsaas.asia>
2024-06-13 00:04:02 +0700

    Add ci

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,26 @@
-FROM node:16-alpine
+# Use the official Node.js 20 image as the base image
+FROM node:20
+
+# Set the working directory
 WORKDIR /app
 
-# Copy only package.json and package-lock.json first
-COPY package*.json ./
-RUN npm install --prefer-offline --frozen-lockfile --non-interactive
+# Copy only the package.json and yarn.lock first to leverage Docker cache
+COPY package.json yarn.lock ./
+
+# Install dependencies using Yarn
+RUN yarn install --frozen-lockfile
 
 # Copy the rest of the application code
 COPY . .
 
-RUN npm run build
+# Build the application
+RUN yarn run build
 
+# Set environment variables
 ENV HOST 0.0.0.0
 
-EXPOSE 80
-CMD ["npm", "run", "deploy:dev"]
+# Expose the port the app runs on
+EXPOSE 8000
+
+# Start the application
+CMD ["yarn", "run", "deploy:dev"]
--- ormconfig.ts
+++ ormconfig.ts
@@ -1,19 +1,25 @@
 import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+import { join } from 'path';
 
-const config = new DataSource({
-  migrationsTableName: 'migrations_typeorm',
+// Load environment variables from .env file
+dotenv.config({
+  path: process.env.NODE_ENV
+    ? `${process.cwd()}/.env.${process.env.NODE_ENV}`
+    : `${process.cwd()}/.env`,
+});
+
+const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  logging: false,
   synchronize: false,
-  migrations: [__dirname + '/**/database/migrations/*{.ts,.js}'],
-  //   namingStrategy: new SnakeNamingStrategy(),
+  logging: false,
+  entities: [join(__dirname, '**/*.entity{.ts,.js}')],
+  migrations: [join(__dirname, '**/migrations/*{.ts,.js}')],
 });
 
-config.initialize();
-
-export default config;
+export default AppDataSource;
